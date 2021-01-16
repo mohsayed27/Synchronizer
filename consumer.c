603add52ff5ae0c9b001f6cb9d34486670699625
mohsayed27 <mohsayed27@gmail.com>
2021-01-16 19:30:48 +0200

    no warning, messaging to be checked

--- consumer.c
+++ consumer.c
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <ctype.h>
+#include <signal.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-#include <signal.h>
+#include <ctype.h>
+#include <errno.h>
 
 #define N 100
 
@@ -59,54 +60,133 @@ void up(int sem_id)
     }
 }
 
-key_t msgq_key_id, shm_key_id, mutex_sem_key_id, cnt_shm_key_id, idx_shm_key_id;
-int msgq_id, send_val, shm_id, rec_val, mutex_sem_id, cnt_shm_id, cns_idx_shm_id;
+key_t msgq_key_id, shm_key_id, mutex_sem_key_id, prc_sem_key_id, cnt_shm_key_id, idx_shm_key_id;
+int msgq_id, send_val, shm_id, rec_val, mutex_sem_id, prc_sem_id, cnt_shm_id, cons_idx_shm_id;
 int *shmaddr;
 int* cnt_ptr;
-int* cns_idx_ptr;
-
+int* cons_idx_ptr;
+int init_cnt = 0, init_cons_idx = 0;
 
 int main()
 {
     signal(SIGINT, handler);
+
+    union Semun semun;
+    struct msgbuff message;
+    message.mtype = 7;
     
+    prc_sem_key_id = ftok("keyfile", 104);
     msgq_key_id = ftok("keyfile", 27);
     shm_key_id = ftok("keyfile", 7);
     mutex_sem_key_id = ftok("keyfile", 2);
     cnt_shm_key_id = ftok("keyfile", 10);
     idx_shm_key_id = ftok("keyfile", 4);
 
-    msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT);
-    shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666);
-    mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT);
-    cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666);
-    cns_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666);
-
-
-    if (msgq_id == -1 || shm_id == -1 || mutex_sem_id == -1 || cnt_shm_id == -1 || cns_idx_shm_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
+    prc_sem_id = semget(prc_sem_key_id, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(prc_sem_id == -1){
+        if(errno == EEXIST){
+            prc_sem_id = semget(prc_sem_key_id, 1, 0666 | IPC_CREAT);
+            up(prc_sem_id);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
     }
+    else{
+        semun.val = 1; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(prc_sem_id, 1, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+    }
+
+
+    msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT | IPC_EXCL);
+    if(msgq_id == -1){
+        if(errno == EEXIST){
+            msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+
+    shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(shm_id == -1){
+        if(errno == EEXIST){
+            shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+
+
+    mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(mutex_sem_id == -1){
+        if(errno == EEXIST){
+            mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        semun.val = 0; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(mutex_sem_id, 1, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+    }
+
+    cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(cnt_shm_id == -1){
+        if(errno == EEXIST){
+            cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        init_cnt = 1;
+    }
+
+    cons_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(cons_idx_shm_id == -1){
+        if(errno == EEXIST){
+            cons_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        init_cons_idx = 1;
+    }
+
+    
     printf("Message Queue ID = %d\n", msgq_id);
     printf("Shared Memory ID = %d\n", shm_id);
-    printf("Semaphor ID = %d\n", mutex_sem_id);
-
-    
-    union Semun semun;
-    struct msgbuff message;
-    message.mtype = 7;
-
+    printf("Semaphor ID = %d\n", mutex_sem_id); 
     
     shmaddr = shmat(shm_id, (void *)0, 0);
-    if (shmaddr == -1)
+    if (shmaddr == NULL)
     {
         perror("Error in attach in Server");
         exit(-1);
     }
     else
     {
-        printf("\nServer: Shared memory attached at address %x\n", shmaddr);
+        printf("\nServer: Shared memory attached at address %ls\n", shmaddr);
     }
 
     cnt_ptr = (int*)shmat(cnt_shm_id, (void *)0, 0);
@@ -117,10 +197,10 @@ int main()
     }
     else
     {
-        printf("\nClient: Shared memory attached at address %x\n", shmaddr);
+        printf("\nClient: Shared memory attached at address %ls\n", shmaddr);
     }
 
-    cns_idx_ptr = shmat(cns_idx_shm_id, (void *)0, 0);
+    cons_idx_ptr = shmat(cons_idx_shm_id, (void *)0, 0);
     if (shmaddr == NULL)
     {
         perror("Error in attach in client");
@@ -128,7 +208,7 @@ int main()
     }
     else
     {
-        printf("\nClient: Shared memory attached at address %x\n", shmaddr);
+        printf("\nClient: Shared memory attached at address %ls\n", shmaddr);
     }
 
     
@@ -137,35 +217,56 @@ int main()
     int *buffer = (int*)shmaddr;
 
     while(1)
-    {
+    {   
         if(0 == *cnt_ptr){
             rec_val = msgrcv(msgq_id, &message, sizeof(message.mtext), message.mtype, !IPC_NOWAIT);
             if (rec_val == -1)
                 perror("Error in receive");
+            else 
+                printf("\nmessage received from producer: %s", message.mtext);
         }
         down(mutex_sem_id);
-        i = *cns_idx_ptr;
-        item = buffer[*cns_idx_ptr];
-        *cns_idx_ptr = i - 1 == -1 ? N - 1 : i - 1;
+        i = *cons_idx_ptr;
+        item = buffer[i];
+        *cons_idx_ptr = i - 1 == -1 ? N - 1 : i - 1;
+        *cnt_ptr -= 1;
         up(mutex_sem_id);
 
-        if(N == *cnt_ptr){
+        if(N - 1 == *cnt_ptr){
+            strcpy(message.mtext, "I have consumed");
             send_val = msgsnd(msgq_id, &message, sizeof(message.mtext), !IPC_NOWAIT);
             if (send_val == -1)
                 perror("Errror in send");
-        }
-
-
+            else
+                printf("\nmessage sent from consumer: %s", message.mtext);
             
+        }
     }
-
     return 0;
 }
 
 
 void handler(int signum)
 {
-    printf("\nAbout to destroy the shared memory area !\n");
-    shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
+    down(prc_sem_id);
+    rec_val = semctl(prc_sem_id, 1, GETVAL);
+    if(rec_val == -1){
+        perror("Error in semctl");
+        exit(-1);
+    }
+    else{
+        if(rec_val == 0){
+            shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(cons_idx_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(cnt_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            semctl(prc_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+            semctl(mutex_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+        }
+        else{
+            shmdt(shmaddr);
+            shmdt(cons_idx_ptr);
+            shmdt(cnt_ptr);
+        }
+    }
     exit(0);
 }
--- producer.c
+++ producer.c
@@ -9,6 +9,7 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define N 100
 
@@ -59,44 +60,123 @@ void up(int sem_id)
     }
 }
 
-key_t msgq_key_id, shm_key_id, mutex_sem_key_id, cnt_shm_key_id, idx_shm_key_id;
-int msgq_id, send_val, shm_id, rec_val, mutex_sem_id, cnt_shm_id, prod_idx_shm_id;
+key_t msgq_key_id, shm_key_id, mutex_sem_key_id, prc_sem_key_id, cnt_shm_key_id, idx_shm_key_id;
+int msgq_id, send_val, shm_id, rec_val, mutex_sem_id, prc_sem_id, cnt_shm_id, prod_idx_shm_id;
 int *shmaddr;
 int* cnt_ptr;
 int* prod_idx_ptr;
-
+int init_cnt = 0, init_prod_idx = 0;
 
 int main()
 {
     signal(SIGINT, handler);
    
+    union Semun semun;
+    struct msgbuff message;
+    message.mtype = 7;
+
+    prc_sem_key_id = ftok("keyfile", 104);
     msgq_key_id = ftok("keyfile", 27);
     shm_key_id = ftok("keyfile", 7);
     mutex_sem_key_id = ftok("keyfile", 2);
     cnt_shm_key_id = ftok("keyfile", 10);
     idx_shm_key_id = ftok("keyfile", 42);
-
-    msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT);
-    shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666);
-    mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT);
-    cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666);
-    prod_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666);
-
-
-    if (msgq_id == -1 || shm_id == -1 || mutex_sem_id == -1 || cnt_shm_id == -1 || prod_idx_shm_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
+     
+    prc_sem_id = semget(prc_sem_key_id, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(prc_sem_id == -1){
+        if(errno == EEXIST){
+            prc_sem_id = semget(prc_sem_key_id, 1, 0666 | IPC_CREAT);
+            up(prc_sem_id);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
     }
+    else{
+        semun.val = 1; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(prc_sem_id, 1, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+    }
+
+
+    msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT | IPC_EXCL);
+    if(msgq_id == -1){
+        if(errno == EEXIST){
+            msgq_id = msgget(msgq_key_id, 0666 | IPC_CREAT);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+
+    shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(shm_id == -1){
+        if(errno == EEXIST){
+            shm_id = shmget(shm_key_id, N * sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+
+
+    mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(mutex_sem_id == -1){
+        if(errno == EEXIST){
+            mutex_sem_id = semget(mutex_sem_key_id, 1, 0666 | IPC_CREAT);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        semun.val = 0; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(mutex_sem_id, 1, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+    }
+
+    cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(cnt_shm_id == -1){
+        if(errno == EEXIST){
+            cnt_shm_id = shmget(cnt_shm_key_id, sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        init_cnt = 1;
+    }
+
+    prod_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666 | IPC_EXCL);
+    if(prod_idx_shm_id == -1){
+        if(errno == EEXIST){
+            prod_idx_shm_id = shmget(idx_shm_key_id, sizeof(int), IPC_CREAT | 0666);
+        }
+        else{
+            perror("Error in create");
+            exit(-1);
+        }
+    }
+    else{
+        init_prod_idx = 1;
+    }
+
+    
     printf("Message Queue ID = %d\n", msgq_id);
     printf("Shared Memory ID = %d\n", shm_id);
     printf("Semaphor ID = %d\n", mutex_sem_id);
-
-    
-    union Semun semun;
-    struct msgbuff message;
-    message.mtype = 7;
-
 
     shmaddr = shmat(shm_id, (void *)0, 0);
     if (shmaddr == NULL)
@@ -106,7 +186,7 @@ int main()
     }
     else
     {
-        printf("\nClient: Shared memory attached at address %x\n", shmaddr);
+        printf("\nClient: Shared memory attached at address %ls\n", shmaddr);
     }
 
     cnt_ptr = (int*)shmat(cnt_shm_id, (void *)0, 0);
@@ -117,7 +197,10 @@ int main()
     }
     else
     {
-        printf("\nClient: Shared memory attached at address %x\n", shmaddr);
+        printf("\nClient: Shared memory attached at address %ls\n", shmaddr);
+    }
+    if(init_cnt){
+        *cnt_ptr = 0;
     }
 
     prod_idx_ptr = shmat(prod_idx_shm_id, (void *)0, 0);
@@ -128,25 +211,12 @@ int main()
     }
     else
     {
-        printf("\nClient: Shared memory attached at address %x\n", shmaddr);
+        printf("\nClient: Shared memory attached at address %ls\n", shmaddr);
     }
-
+    if(init_prod_idx){
+        *prod_idx_ptr = 0;
+    }
     
-    int val = semctl(mutex_sem_id, 1, GETVAL, semun);
-    if (val == -1){
-        perror("Error in semctl");
-        exit(-1);
-    }
-    else{
-        printf("\nSem Value is %d", val);
-    }
-
-    semun.val = 0; /* initial value of the semaphore, Binary semaphore */
-    if (semctl(mutex_sem_id, 1, SETVAL, semun) == -1)
-    {
-        perror("Error in semctl");
-        exit(-1);
-    }
 
     int item;
     int prod_cnt = 0;
@@ -154,12 +224,13 @@ int main()
     int i;
 
     while(1){
-        //break;
         item = prod_cnt + 1;
         if(N == *cnt_ptr){
             rec_val = msgrcv(msgq_id, &message, sizeof(message.mtext), message.mtype, !IPC_NOWAIT);
             if (rec_val == -1)
                 perror("Error in receive");
+            else 
+                printf("\nmessage received from consumer: %s", message.mtext);
         }
         down(mutex_sem_id);
         i = *prod_idx_ptr;
@@ -169,6 +240,7 @@ int main()
         up(mutex_sem_id);
 
         if(1 == *cnt_ptr){
+            strcpy(message.mtext, "I have produced");
             send_val = msgsnd(msgq_id, &message, sizeof(message.mtext), !IPC_NOWAIT);
             if (send_val == -1)
                 perror("Errror in send");
@@ -176,13 +248,51 @@ int main()
 
     }
 
+    down(prc_sem_id);
+    rec_val = semctl(prc_sem_id, 1, GETVAL);
+    if(rec_val == -1){
+        perror("Error in semctl");
+        exit(-1);
+    }
+    else{
+        if(rec_val == 0){
+            shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(prod_idx_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(cnt_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            semctl(prc_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+            semctl(mutex_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+        }
+        else{
+            shmdt(shmaddr);
+            shmdt(prod_idx_ptr);
+            shmdt(cnt_ptr);
+        }
+    }
     return 0;
 }
 
 
 void handler(int signum)
 {
-    printf("\nClient Detaching...");
-    shmdt(shmaddr);
+    down(prc_sem_id);
+    rec_val = semctl(prc_sem_id, 1, GETVAL);
+    if(rec_val == -1){
+        perror("Error in semctl");
+        exit(-1);
+    }
+    else{
+        if(rec_val == 0){
+            shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(prod_idx_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            shmctl(cnt_shm_id, IPC_RMID, (struct shmid_ds *)0);
+            semctl(prc_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+            semctl(mutex_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+        }
+        else{
+            shmdt(shmaddr);
+            shmdt(prod_idx_ptr);
+            shmdt(cnt_ptr);
+        }
+    }
     exit(0);
 }
